be06b8f17a5f4e751282cc7e567d0871256c8ec9
Alberina <44498537+Alberina@users.noreply.github.com>
2023-04-02 11:44:48 +0200

    add helper functions

--- scripts/buy-coffe.js
+++ scripts/buy-coffe.js
@@ -6,6 +6,29 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
+async function getBalance(address){
+  const balanceBigInt = await hre.waffle.provider.getBalance(address);
+  return hre.ethers.utils.formatEther(balanceBigInt);
+}
+
+async function printBalances(addresses) {
+  let idx = 0;
+  for(const address of addresses){
+    console.log(`Address ${idx} balance: `, await getBalance(address));
+    idx++;
+  }
+}
+
+async function printMemos(memos){
+  for(const memo of memos){
+    const timestamp = memo.timestamp;
+    const tipper = memo.name;
+    const tipperAddress = memo.from;
+    const message = memo.message;
+    console.log(`At ${message}, ${tipper},(${tipperAddress}) said: "${message}"`);
+  }
+}
+
 async function main() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
   const unlockTime = currentTimestampInSeconds + 60;
